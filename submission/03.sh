2f5ca9d58ed0854630d695242625bbc85635a07a
b-l-u-e <winnie.gitau282@gmail.com>
2025-03-16 08:28:55 +0300

    fix:add -rpcwallet flag to generateaddress call in 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -2,7 +2,7 @@
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
 # Add funds to the address.
-bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS"
+bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 "$ADDRESS"
 
 # Return only the Address
 echo "$ADDRESS"
